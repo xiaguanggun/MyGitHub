940f354a526fbfcfb25d58019eda780e5c71bb02
xiaguanggun <xiaguanggun@gmail.com>
2024-03-24 23:23:08 +0800

    xgg

---
+++ 2_Linux/1_makefile/Makefile1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int add(int num1, int num2);
+int sub(int num1, int num2);
+
+int main()
+{
+    printf("add(3,4) = %d\n", add(3, 4));
+    printf("sub(3,4) = %d\n", sub(3, 4));
+    return 0;
+}
+
