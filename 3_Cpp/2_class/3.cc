070d6243f77b0a554543510fdca262b040f2caf2
xiaguanggun <xiaguanggun@gmail.com>
2024-03-28 16:54:52 +0800

    cpp/2_class

---
+++ 3_Cpp/2_class/3.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class B
+{
+public:
+	B()
+	{
+        cout << "B()" << endl;
+    }
+
+    ~B()
+    {
+    	cout << "~B()" << endl;
+    }
+    
+    B(const B &rhs)
+    {
+        cout << "B(const B&)" << endl;
+    }
+    
+    B &operator=(const B &rhs)
+    {
+    	cout << "B &operator=(const B &s)" << endl;
+    
+        return  *this;
+    }
+};
+
+B func(const B &rhs)
+{
+    cout << "B func(const B &)" << endl;
+    return rhs;
+}
+
+
+int main()
+{
+    B b1,b2;
+    b2=func(b1);//10#
+    return 0;
+}
