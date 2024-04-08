2f83e34a77a307b9d135d3d26e8bd6b1218622a3
xiaguanggun <xiaguanggun@gmail.com>
2024-04-08 14:27:18 +0800

    m cpp/mystring

--- 3_Cpp/mystring/myString.cc
+++ 3_Cpp/mystring/myString.cc
@@ -3,25 +3,126 @@
 #include "myString.h"
 using namespace std;
 
+// 辅助函数
+void String::initRefCount(){
+    *(std::size_t *)(_pstr-krefcountsize) = 1;
+}
+void String::increseRefCount(){
+    ++*(std::size_t *)(_pstr-krefcountsize);
+}
+void String::decreseRefCount(){
+    --*(std::size_t *)(_pstr-krefcountsize);
+}
+void String::release(){
+    if(getRefCount() == 1){
+        delete [] (_pstr-krefcountsize);
+        _pstr = nullptr;
+    }
+    else{
+        decreseRefCount();
+    }
+}
+char * String::malloc(){
+    return (new char[_size+1+krefcountsize]() + krefcountsize);
+}
+// 内部类
+String::InClass::InClass(std::size_t index,String *ps):_index(index),_ps(ps){}
+//String::InClass::~InClass(){}
+//String::InClass & String::InClass::operator=(char ch){
+//    // 验证index
+//    if((_index < 0) || _index > _ps->_size){
+//        cout << "index out of range!" << endl;
+//        static String::InClass nullInClase(0,new String());
+//        return nullInClase;
+//    }
+//    if(_ps->getRefCount() > 1){
+//        char *temp = _ps->malloc();
+//        strcpy(temp,_ps->_pstr);
+//        _ps->decreseRefCount();
+//        _ps->_pstr = temp;
+//        _ps->initRefCount();
+//    }
+//    _ps->_pstr[_index] = ch;
+//    return *this;
+//}
+//String::InClass & String::InClass::operator=(const InClass &rhs){
+//    // 验证index
+//    if((_index < 0) || _index > _ps->_size){
+//        cout << "index out of range!" << endl;
+//        static String::InClass nullInClase(0,new String());
+//        return nullInClase;
+//    }
+//    if(_ps->getRefCount() > 1){
+//        char *temp = _ps->malloc();
+//        strcpy(temp,_ps->_pstr);
+//        _ps->decreseRefCount();
+//        _ps->_pstr = temp;
+//        _ps->initRefCount();
+//    }
+//    _ps->_pstr[_index] = rhs._ps->_pstr[rhs._index];
+//    return *this;
+//}
+char & String::InClass::operator=(char ch){
+    // 验证index
+    if((_index < 0) || _index > _ps->_size){
+        cout << "index out of range!" << endl;
+        static char nullchar = '\0';
+        return nullchar;
+    }
+    if(_ps->getRefCount() > 1){
+        char *temp = _ps->malloc();
+        strcpy(temp,_ps->_pstr);
+        _ps->decreseRefCount();
+        _ps->_pstr = temp;
+        _ps->initRefCount();
+    }
+    return _ps->_pstr[_index] = ch;
+}
+String::InClass::operator char(){
+    // 验证index
+    if((_index < 0) || _index > _ps->_size){
+        cout << "index out of range!" << endl;
+    }
+    //cout << "operator char" << endl;
+    return _ps->_pstr[_index];
+}
+//std::ostream & operator<<
+//    (std::ostream &os, const String::InClass &si)
+//{
+//    // 验证index
+//    if((si._index < 0) || si._index > si._ps->_size){
+//        cout << "index out of range!" << endl;
+//    }
+//    else{
+//        os << si._ps->_pstr[si._index];
+//    }
+//    return os;
+//}
+
+
+// String类
+// 静态变量
+std::size_t String::krefcountsize = sizeof(std::size_t);
 // 构造 + 析构
-String::String():_size(0),_pstr(new char[1]()){}
+String::String():_size(0),_pstr(malloc())
+{
+    initRefCount();
+}
 String::String(const char *cstr):
     _size(strlen(cstr)),
-    _pstr(new char[_size+1]())
+    _pstr(malloc())
 {
     memcpy(_pstr,cstr,_size);
+    initRefCount();
 }
 String::String(const String &rhs):
     _size(rhs._size),
-    _pstr(new char[_size+1]())
+    _pstr(rhs._pstr)
 {
-    memcpy(_pstr,rhs._pstr,_size);
+    increseRefCount();
 }
 String::~String(){
-    if(_pstr){
-        delete [] _pstr;
-        _pstr = nullptr;
-    }
+    release();
 }
 // 赋值运算符
 String & String::operator=(const String &rhs){
@@ -30,45 +131,42 @@ String & String::operator=(const String &rhs){
         return *this;
     }
     _size = rhs._size;
-    delete [] _pstr;
-    _pstr = new char[_size+1]();
-    memcpy(_pstr,rhs._pstr,_size);
+    _pstr = rhs._pstr;
+    increseRefCount();
     return *this;
 }
 String & String::operator=(const char *cstr){
+    release();
     _size = strlen(cstr);
-    delete [] _pstr;
-    _pstr = new char[_size+1]();
+    _pstr = malloc();
     memcpy(_pstr,cstr,_size);
+    initRefCount();
     return *this;
 }
 String & String::operator+=(const String &rhs){
     _size += rhs._size;
-    char *temp = new char[_size+1]();
+    char *temp = malloc();
     strcpy(temp,_pstr);
     strcat(temp,rhs._pstr);
-    delete [] _pstr;
+    release();
     _pstr = temp;
+    initRefCount();
     return *this;
 }
 String & String::operator+=(const char *cstr){
     _size += strlen(cstr);
-    char *temp = new char[_size+1]();
+    char *temp = malloc();
     strcpy(temp,_pstr);
     strcat(temp,cstr);
-    delete [] _pstr;
+    release();
     _pstr = temp;
+    initRefCount();
     return *this;
 }
 // []
-char & String::operator[](std::size_t index){
-    // 验证index
-    if(index < 0 || index > _size){
-        cout << "index out of range!" << endl;
-        static char nullchar = '\0';
-        return nullchar;
-    }
-    return _pstr[index];
+String::InClass String::operator[](std::size_t index){
+    // 使用内部类
+    return InClass(index,this);
 }
 const char & String::operator[](std::size_t index) const{
     // 验证index
@@ -85,6 +183,9 @@ std::size_t String::size() const{
 }
 const char* String::c_str() const{
     return _pstr;
+}
+const std::size_t & String::getRefCount() const{
+    return *(std::size_t *)(_pstr-krefcountsize);
 }
 // 比较运算符
 bool operator==(const String &lhs, const String &rhs){
@@ -117,9 +218,10 @@ std::istream & operator>>(std::istream &is, String &s){
         temp.push_back(ch);
     }
     s._size = temp.size();
-    delete [] s._pstr;
-    s._pstr = new char[s._size+1]();
+    s.release();
+    s._pstr = s.malloc();
     memcpy(s._pstr,&temp[0],s._size);
+    s.initRefCount();
     return is;
 }
 // 加法拼接运算符
--- 3_Cpp/mystring/myString.h
+++ 3_Cpp/mystring/myString.h
@@ -5,6 +5,27 @@
 
 class String 
 {
+private:
+    // 内部类
+    class InClass{
+    public:
+        InClass(std::size_t index,String *ps);
+        //~InClass();
+        static void deleteSi();
+        //InClass &operator=(char);
+        //InClass &operator=(const InClass &);
+        char &operator=(char);
+        // 类型转换函数,返回一个副本,可以传参、读
+        // ostream自动转换,不需要重载<<
+        operator char();
+        //friend std::ostream &operator<<(std::ostream &os, const InClass &si);
+    private:
+        std::size_t _index;
+        String *_ps;
+    };
+//public:
+    // 内部类友元函数
+    //friend std::ostream &operator<<(std::ostream &os, const InClass &si);
 public:
     // 构造 + 析构
     String();
@@ -17,11 +38,12 @@ public:
     String &operator+=(const String &);
     String &operator+=(const char *);
     // []
-    char &operator[](std::size_t index);
+    InClass operator[](std::size_t index);
     const char &operator[](std::size_t index) const;
     // 成员函数
     std::size_t size() const;
     const char* c_str() const;
+    const std::size_t & getRefCount() const;
     // 比较运算符
     friend bool operator==(const String &, const String &);
     friend bool operator!=(const String &, const String &);
@@ -36,24 +58,18 @@ public:
     friend String operator+(const String &, const String &);
     friend String operator+(const String &, const char *);
     friend String operator+(const char *, const String &);
-
 private:
     std::size_t _size;
     char * _pstr;
+    //InClass _si;
+    static std::size_t krefcountsize;
+private:
+    // 辅助函数
+    void initRefCount();
+    void increseRefCount();
+    void decreseRefCount();
+    void release();
+    char * malloc();
 };
-// 比较运算符
-bool operator==(const String &, const String &);
-bool operator!=(const String &, const String &);
-bool operator<(const String &, const String &);
-bool operator>(const String &, const String &);
-bool operator<=(const String &, const String &);
-bool operator>=(const String &, const String &);
-// 输入输出运算符
-std::ostream &operator<<(std::ostream &os, const String &s);
-std::istream &operator>>(std::istream &is, String &s);
-// 加法拼接运算符
-String operator+(const String &, const String &);
-String operator+(const String &, const char *);
-String operator+(const char *, const String &);
 
 #endif
